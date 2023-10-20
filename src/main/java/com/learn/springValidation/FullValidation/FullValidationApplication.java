5674ac229f3ccc50427fb7ae3667c71c9879edae
Barungit <barunkr0801@gmail.com>
2023-10-20 16:50:24 +0530

    First commit

---
+++ src/main/java/com/learn/springValidation/FullValidation/FullValidationApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springValidation.FullValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullValidationApplication.class, args);
+	}
+
+}
