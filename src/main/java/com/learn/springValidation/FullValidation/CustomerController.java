fa2e116084f5fbf5501a0f23b6719c57a2767aa0
Barungit <barunkr0801@gmail.com>
2023-10-22 19:47:42 +0530

    Added Customer-Form with validation constraints and CustomerController classe to validate data

--- src/main/java/com/learn/springValidation/FullValidation/CustomerController.java
+++ src/main/java/com/learn/springValidation/FullValidation/CustomerController.java
@@ -2,7 +2,12 @@ package com.learn.springValidation.FullValidation;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
@@ -12,5 +17,17 @@ public class CustomerController {
 		
 		theModel.addAttribute("customer", new Customer());
 		return "customer-form";
+	}
+	
+	@PostMapping("/processForm")
+	public String processForm(
+			@Valid @ModelAttribute("customer") Customer thecustomer,
+			BindingResult theBindingResult) {
+		thecustomer.setLastName(thecustomer.getLastName().trim());
+		if(theBindingResult.hasErrors()) {
+			return "customer-form";
+		} else {
+			return "customer-confirmation";
+		}
 	}
 }
