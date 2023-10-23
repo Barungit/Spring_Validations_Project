d8a091a1db1bfd7bff7ef20ef9221cb3c4bacdc4
Barungit <barunkr0801@gmail.com>
2023-10-23 13:58:07 +0530

    used initBinder to resolve the issue of empty strings passing validations

--- src/main/java/com/learn/springValidation/FullValidation/CustomerController.java
+++ src/main/java/com/learn/springValidation/FullValidation/CustomerController.java
@@ -1,9 +1,12 @@
 package com.learn.springValidation.FullValidation;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -11,6 +14,13 @@ import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 	
 	@GetMapping("/")
 	public String showForm(Model theModel) {
@@ -23,7 +33,6 @@ public class CustomerController {
 	public String processForm(
 			@Valid @ModelAttribute("customer") Customer thecustomer,
 			BindingResult theBindingResult) {
-		thecustomer.setLastName(thecustomer.getLastName().trim());
 		if(theBindingResult.hasErrors()) {
 			return "customer-form";
 		} else {
