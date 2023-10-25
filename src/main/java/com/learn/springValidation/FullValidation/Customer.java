e0c7ce39a454a1cfc5ec42905223788aa93d53f6
Barungit <barunkr0801@gmail.com>
2023-10-25 11:13:06 +0530

    added Number range validation by min max and regex

--- src/main/java/com/learn/springValidation/FullValidation/Customer.java
+++ src/main/java/com/learn/springValidation/FullValidation/Customer.java
@@ -1,6 +1,9 @@
 package com.learn.springValidation.FullValidation;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
@@ -10,6 +13,29 @@ public class Customer {
 	@NotNull(message = "is required")
 	@Size(min = 1, message = "is required")
 	private String lastName;
+	
+	@Min(value = 0, message = "must be greater than or equal to 0")
+	@Max(value = 10, message = "must be less than or equal to 10")
+	private int freePasses;
+	
+	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only five digits/digits")
+	private String postalCode;
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
 
 	public String getFirstName() {
 		return firstName;
